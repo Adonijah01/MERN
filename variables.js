6f0d959565d49de1802534fa6318d021a79eba80
adonijah01 <adonijahsingoei@gmail.com>
2023-09-06 18:36:24 +0300

    MERN Learning

---
+++ variables.js
@@ -0,0 +1,14 @@
+// Declare variables of different data types
+var numberVariable = 42; // Numeric variable
+var stringVariable = "Hello, world!"; // String variable
+var booleanVariable = true; // Boolean variable
+var nullVariable = null; // Null variable
+var undefinedVariable; // Undefined variable
+
+// Print variable values to the console
+console.log("Number Variable:", numberVariable);
+console.log("String Variable:", stringVariable);
+console.log("Boolean Variable:", booleanVariable);
+console.log("Null Variable:", nullVariable);
+console.log("Undefined Variable:", undefinedVariable);
+
